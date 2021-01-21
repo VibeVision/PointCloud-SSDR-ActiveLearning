a09fd0d1a96e48f75732218682a6440a34397761
VibeVision <VibeVisio80@proton.me>
2021-01-21 06:43:18 +0000

    0.1.23

--- SSDR_AL_s3dis/base_op.py
+++ SSDR_AL_s3dis/base_op.py
@@ -5,4 +5,12 @@ def get_sampler_args_str(sampler_args):
         return ""
 
     sampler_name = ""
-    
+    for element in sampler_args:
+        sampler_name = sampler_name + element + "-"
+    return sampler_name[:-1]
+
+def get_w(w):
+    s = ""
+    for key in w:
+        s = s + ", " + key + "=" + str(w[key])
+    return s
---
+++ SSDR_AL_s3dis/helper_tool.py
@@ -0,0 +1,266 @@
+
+from open3d import linux as open3d
+from os.path import join
+import numpy as np
+import colorsys, random, os, sys
+import pandas as pd
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, 'utils'))
+
+import cpp_wrappers.cpp_subsampling.grid_subsampling as cpp_subsampling
+import nearest_neighbors.lib.python.nearest_neighbors as nearest_neighbors
+
+
+class ConfigSemanticKITTI:
+    k_n = 16  # KNN
+    num_layers = 4  # Number of layers
+    num_points = 4096 * 11  # Number of input points
+    num_classes = 19  # Number of valid classes
+    sub_grid_size = 0.06  # preprocess_parameter
+
+    batch_size = 6  # batch_size during training
+    val_batch_size = 20  # batch_size during validation and test
+    train_steps = 500  # Number of steps per epochs
+    val_steps = 100  # Number of validation steps per epoch
+
+    sub_sampling_ratio = [4, 4, 4, 4]  # sampling ratio of random sampling at each layer
+    d_out = [16, 64, 128, 256]  # feature dimension
+    num_sub_points = [num_points // 4, num_points // 16, num_points // 64, num_points // 256]
+
+    noise_init = 3.5  # noise initial parameter
+    max_epoch = 100  # maximum epoch during training
+    learning_rate = 1e-2  # initial learning rate
+    lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
+
+    train_sum_dir = 'train_log'
+    saving = True
+    saving_path = None
+
+    # add by ssdr
+    ignored_label_inds = [0]
+
+class ConfigS3DIS:
+    k_n = 16  # KNN
+    num_layers = 5  # Number of layers
+    num_points = 40960  # Number of input points
+    num_classes = 13  # Number of valid classes
+    sub_grid_size = 0.04  # preprocess_parameter
+
+    batch_size = 6  # batch_size during training
+    val_batch_size = 20  # batch_size during validation and test
+    train_steps = 500  # Number of steps per epochs
+    val_steps = 100  # Number of validation steps per epoch
+
+    sub_sampling_ratio = [4, 4, 4, 4, 2]  # sampling ratio of random sampling at each layer
+    d_out = [16, 64, 128, 256, 512]  # feature dimension
+
+    noise_init = 3.5  # noise initial parameter
+    # max_epoch = 100  # maximum epoch during training
+    max_epoch = 30  # maximum epoch during training   0.84 ** 30 == 0.95 ** 100
+    # max_epoch = 50
+
+    learning_rate = 1e-2  # initial learning rate
+    # lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
+    lr_decays = {i: 0.84 for i in range(0, 500)}  # decay rate of learning rate
+    # lr_decays = {i: 0.9 for i in range(0, 500)}  # decay rate of learning rate
+
+    train_sum_dir = 'train_log'
+    saving = True
+    saving_path = None
+    # add by ssdr
+    ignored_label_inds = []
+
+class ConfigSemantic3D:
+    k_n = 16  # KNN
+    num_layers = 5  # Number of layers
+    num_points = 65536  # Number of input points
+    num_classes = 8  # Number of valid classes
+    sub_grid_size = 0.06  # preprocess_parameter
+
+    batch_size = 4  # batch_size during training
+    val_batch_size = 16  # batch_size during validation and test
+    train_steps = 500  # Number of steps per epochs
+    val_steps = 100  # Number of validation steps per epoch
+    # val_steps = 40  # Number of validation steps per epoch
+    ### add ssdr
+    # test_batch_size = 16  # batch_size during validation and test
+    # test_steps = 100  # Number of validation steps per epoch
+    ###
+
+    sub_sampling_ratio = [4, 4, 4, 4, 2]  # sampling ratio of random sampling at each layer
+    d_out = [16, 64, 128, 256, 512]  # feature dimension
+
+    noise_init = 3.5  # noise initial parameter
+    # max_epoch = 100  # maximum epoch during training
+    max_epoch = 50  # maximum epoch during training   0.84 ** 30 == 0.95 ** 100
+    learning_rate = 1e-2  # initial learning rate
+    # lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
+    lr_decays = {i: 0.9 for i in range(0, 500)}  # decay rate of learning rate
+
+    train_sum_dir = 'train_log'
+    saving = True
+    saving_path = None
+
+    augment_scale_anisotropic = True
+    augment_symmetries = [True, False, False]
+    augment_rotation = 'vertical'
+    augment_scale_min = 0.8
+    augment_scale_max = 1.2
+    augment_noise = 0.001
+    augment_occlusion = 'none'
+    augment_color = 0.8
+    # add by ssdr
+    ignored_label_inds = [0]
+
+class DataProcessing:
+    @staticmethod
+    def load_pc_semantic3d(filename):
+        pc_pd = pd.read_csv(filename, header=None, delim_whitespace=True, dtype=np.float16)
+        pc = pc_pd.values
+        return pc
+
+    @staticmethod
+    def load_label_semantic3d(filename):
+        label_pd = pd.read_csv(filename, header=None, delim_whitespace=True, dtype=np.uint8)
+        cloud_labels = label_pd.values
+        return cloud_labels
+
+    @staticmethod
+    def load_pc_kitti(pc_path):
+        scan = np.fromfile(pc_path, dtype=np.float32)
+        scan = scan.reshape((-1, 4))
+        points = scan[:, 0:3]  # get xyz
+        return points
+
+    @staticmethod
+    def load_label_kitti(label_path, remap_lut):
+        label = np.fromfile(label_path, dtype=np.uint32)
+        label = label.reshape((-1))
+        sem_label = label & 0xFFFF  # semantic label in lower half
+        inst_label = label >> 16  # instance id in upper half
+        assert ((sem_label + (inst_label << 16) == label).all())
+        sem_label = remap_lut[sem_label]
+        return sem_label.astype(np.int32)
+
+    @staticmethod
+    def get_file_list(dataset_path, test_scan_num):
+        seq_list = np.sort(os.listdir(dataset_path))
+
+        train_file_list = []
+        test_file_list = []
+        val_file_list = []
+        for seq_id in seq_list:
+            seq_path = join(dataset_path, seq_id)
+            pc_path = join(seq_path, 'velodyne')
+            if seq_id == '08':
+                val_file_list.append([join(pc_path, f) for f in np.sort(os.listdir(pc_path))])
+                if seq_id == test_scan_num:
+                    test_file_list.append([join(pc_path, f) for f in np.sort(os.listdir(pc_path))])
+            elif int(seq_id) >= 11 and seq_id == test_scan_num:
+                test_file_list.append([join(pc_path, f) for f in np.sort(os.listdir(pc_path))])
+            elif seq_id in ['00', '01', '02', '03', '04', '05', '06', '07', '09', '10']:
+                train_file_list.append([join(pc_path, f) for f in np.sort(os.listdir(pc_path))])
+
+        train_file_list = np.concatenate(train_file_list, axis=0)
+        val_file_list = np.concatenate(val_file_list, axis=0)
+        test_file_list = np.concatenate(test_file_list, axis=0)
+        return train_file_list, val_file_list, test_file_list
+
+    @staticmethod
+    def knn_search(support_pts, query_pts, k):
+        """
+        :param support_pts: points you have, B*N1*3
+        :param query_pts: points you want to know the neighbour index, B*N2*3
+        :param k: Number of neighbours in knn search
+        :return: neighbor_idx: neighboring points indexes, B*N2*k
+        """
+
+        neighbor_idx = nearest_neighbors.knn_batch(support_pts, query_pts, k, omp=True)
+        return neighbor_idx.astype(np.int32)
+
+    @staticmethod
+    def data_aug(xyz, color, labels, activation, pseudo, idx, num_out):
+
+        num_in = len(xyz)
+        dup = np.random.choice(num_in, num_out - num_in)
+        xyz_dup = xyz[dup, ...]
+        xyz_aug = np.concatenate([xyz, xyz_dup], 0)
+        color_dup = color[dup, ...]
+        color_aug = np.concatenate([color, color_dup], 0)
+        idx_dup = list(range(num_in)) + list(dup)
+        idx_aug = idx[idx_dup]
+        label_aug = labels[idx_dup]
+        activation_aug = activation[idx_dup]
+        pseudo_aug = pseudo[idx_dup]
+        return xyz_aug, color_aug, idx_aug, label_aug, activation_aug, pseudo_aug
+
+    @staticmethod
+    def shuffle_idx(x):
+        # random shuffle the index
+        idx = np.arange(len(x))
+        np.random.shuffle(idx)
+        return x[idx]
+
+    @staticmethod
+    def shuffle_list(data_list):
+        indices = np.arange(np.shape(data_list)[0])
+        np.random.shuffle(indices)
+        data_list = data_list[indices]
+        return data_list
+
+    @staticmethod
+    def grid_sub_sampling(points, features=None, labels=None, grid_size=0.1, verbose=0):
+        """
+        CPP wrapper for a grid sub_sampling (method = barycenter for points and features
+        :param points: (N, 3) matrix of input points
+        :param features: optional (N, d) matrix of features (floating number)
+        :param labels: optional (N,) matrix of integer labels
+        :param grid_size: parameter defining the size of grid voxels
+        :param verbose: 1 to display
+        :return: sub_sampled points, with features and/or labels depending of the input
+        """
+
+        if (features is None) and (labels is None):
+            return cpp_subsampling.compute(points, sampleDl=grid_size, verbose=verbose)
+        elif labels is None:
+            return cpp_subsampling.compute(points, features=features, sampleDl=grid_size, verbose=verbose)
+        elif features is None:
+            return cpp_subsampling.compute(points, classes=labels, sampleDl=grid_size, verbose=verbose)
+        else:
+            return cpp_subsampling.compute(points, features=features, classes=labels, sampleDl=grid_size,
+                                           verbose=verbose)
+
+    @staticmethod
+    def IoU_from_confusions(confusions):
+        """
+        Computes IoU from confusion matrices.
+        :param confusions: ([..., n_c, n_c] np.int32). Can be any dimension, the confusion matrices should be described by
+        the last axes. n_c = number of classes
+        :return: ([..., n_c] np.float32) IoU score
+        """
+
+        # Compute TP, FP, FN. This assume that the second to last axis counts the truths (like the first axis of a
+        # confusion matrix), and that the last axis counts the predictions (like the second axis of a confusion matrix)
+        TP = np.diagonal(confusions, axis1=-2, axis2=-1)
+        TP_plus_FN = np.sum(confusions, axis=-1)
+        TP_plus_FP = np.sum(confusions, axis=-2)
+
+        # Compute IoU
+        IoU = TP / (TP_plus_FP + TP_plus_FN - TP + 1e-6)
+
+        # Compute mIoU with only the actual classes
+        mask = TP_plus_FN < 1e-3
+        counts = np.sum(1 - mask, axis=-1, keepdims=True)
+        mIoU = np.sum(IoU, axis=-1, keepdims=True) / (counts + 1e-6)
+
+        # If class is absent, place mIoU in place of 0 IoU to get the actual mean later
+        IoU += mask * mIoU
+        return IoU
+
+    @staticmethod
+    def get_class_weights(dataset_name):
