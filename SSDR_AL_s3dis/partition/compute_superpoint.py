386fd3e18a97cefac2c1aacb68dd39f5e1f31fe3
VibeVision <VibeVisio80@proton.me>
2021-01-24 08:33:23 +0000

    First upload.

--- SSDR_AL_s3dis/partition/compute_superpoint.py
+++ SSDR_AL_s3dis/partition/compute_superpoint.py
@@ -52,4 +52,55 @@ def s3dis_superpoint(args, val_split):
             # --compute the partition------
             # --- build the spg h5 file --
             features = np.hstack((geof, rgb)).astype('float32')  # add rgb as a feature for partitioning
-   
+            features[:, 3] = 2. * features[:, 3]  # increase importance of verticality (heuristic)
+
+            graph_nn["edge_weight"] = np.array(
+                1. / (args.lambda_edge_weight + graph_nn["distances"] / np.mean(graph_nn["distances"])),
+                dtype='float32')
+            print("minimal partition...")
+
+
+            components, in_component = libcp.cutpursuit(features, graph_nn["source"], graph_nn["target"]
+                                                        , graph_nn["edge_weight"], args.reg_strength)
+            components = np.array(components, dtype='object')
+            sp = {}
+            sp["components"] = components
+            sp["in_component"] = in_component
+            with open(os.path.join(output_dir, cloud_name+".superpoint"),"wb") as f:
+                pickle.dump(sp, f)
+
+            pseudo_gt = np.zeros([2, len(xyz)], dtype=np.float32)
+            with open(os.path.join(output_dir, cloud_name+".gt"), "wb") as f:
+                pickle.dump(pseudo_gt, f)
+
+            sp_num = sp_num + len(components)
+            file_num = file_num + 1
+            point_num = point_num + len(xyz)
+
+            total_obj["unlabeled"][cloud_name] = np.arange(len(components))
+
+    total_obj["file_num"] = file_num
+    total_obj["sp_num"] = sp_num
+    total_obj["point_num"] = point_num
+
+    with open(os.path.join(output_dir, "total.pkl"), "wb") as f:
+        pickle.dump(total_obj, f)
+
+    print("file_num", file_num, "sp_num", sp_num, "point_num", point_num)
+
+
+def test_superpoint_distribution(args):
+    all_files = glob.glob(os.path.join('data/S3DIS', str(args.reg_strength), 'superpoint', '*.superpoint'))
+    sp_count = 0
+    point_count = 0
+
+    dis = np.zeros([10000])
+
+    for i, file_path in enumerate(all_files):
+        with open(file_path, "rb") as f:
+            superpoint = pickle.load(f)
+        components = superpoint["components"]
+        sp_count = sp_count + len(components)
+        for sp in components:
+            sp_size = len(sp)
+            point_count = point_count
