1d0bd5a38417a33b750613901bd3f27e2ed59de9
VibeVision <VibeVisio80@proton.me>
2021-03-11 06:42:03 +0000

    Version 1.4.2

---
+++ SSDR_AL_s3dis/partition/cut-pursuit/include/API.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <omp.h>
+#include "CutPursuit_L2.h"
+#include "CutPursuit_Linear.h"
+#include "CutPursuit_KL.h"
+#include "CutPursuit_SPG.h"
+//**********************************************************************************
+//*******************************L0-CUT PURSUIT*************************************
+//**********************************************************************************
+//Greedy graph cut based algorithm to solve the generalized minimal
+//partition problem
+//
+//Cut Pursuit: fast algorithms to learn piecewise constant functions on
+//general weighted graphs, Loic Landrieu and Guillaume Obozinski,2016.
+//
+//Produce a piecewise constant approximation of signal $y$ structured
+//by the graph G=(V,e,mu,w) with mu the node weight and w the edgeweight:
+//argmin \sum_{i \IN V}{mu_i * phi(x_I, y_I)}
+//+ \sum_{(i,j) \IN E}{w_{i,j} 1(x_I != x_J)}
+//
+//phi(X,Y) the fidelity function (3 are implemented)
+//(x != y) the function equal to 1 if x!=y and 0 else
+//
+// LOIC LANDRIEU 2017
+//
+//=======================SYNTAX===================================================
+//---------------REGULARIZATION---------------------------------------------------
+//C style inputs
+//void cut_pursuit(const uint32_t n_nodes, const uint32_t n_edges, const uint32_t nObs
+//          ,const T * 
