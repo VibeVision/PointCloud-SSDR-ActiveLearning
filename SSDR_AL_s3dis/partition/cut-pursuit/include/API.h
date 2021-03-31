b19bce3b85671b9cf8e10307494153df94770a01
VibeVision <VibeVisio80@proton.me>
2021-03-31 16:30:16 +0000

    Delete avatar.jpg

--- SSDR_AL_s3dis/partition/cut-pursuit/include/API.h
+++ SSDR_AL_s3dis/partition/cut-pursuit/include/API.h
@@ -258,4 +258,52 @@ void cut_pursuit(const uint32_t n_nodes, const uint32_t n_edges, const uint32_t 
     ite_nod = boost::vertices(cp->reduced_graph).first;
     for(uint32_t ind_nod_red = 0; ind_nod_red < n_nodes_red; ind_nod_red++ )
     {
-	nodeWeight_red[ind_nod_red] = vertex_attribute_map_red[*
+	nodeWeight_red[ind_nod_red] = vertex_attribute_map_red[*ite_nod].weight;
+        ite_nod++;
+    }
+    EdgeIterator<T> ite_edg = boost::edges(cp->reduced_graph).first;
+    for(uint32_t ind_edg = 0; ind_edg < n_edges_red; ind_edg++ )
+    {    
+	edgeWeight_red[ind_edg] = edge_attribute_map_red[*ite_edg].weight;
+	Eu_red[ind_edg] = vertex_index_map_red(boost::source(*ite_edg, cp->reduced_graph));
+	Ev_red[ind_edg] = vertex_index_map_red(boost::target(*ite_edg, cp->reduced_graph));        
+        ite_edg++;
+    }
+    delete cp;
+    return;
+}
+
+
+
+//===========================================================================
+//=====================  cut_pursuit  C-style  ==============================
+//===========================================================================
+template<typename T>
+void cut_pursuit(const uint32_t n_nodes, const uint32_t n_edges, const uint32_t nObs
+          ,const T * observation, const uint32_t * Eu, const uint32_t * Ev
+          ,const T * edgeWeight, const T * nodeWeight
+          ,T * solution, const T lambda, const uint32_t cutoff, const T mode, const T speed, const T weight_decay
+          , const float verbose)
+{   //C-style interface
+    std::srand (1);
+    if (verbose > 0)
+    {
+        std::cout << "L0-CUT PURSUIT";
+    }
+    //--------parameterization---------------------------------------------
+    CP::CutPursuit<T> * cp = create_CP(mode, verbose);
+    set_speed(cp, speed, weight_decay, verbose);
+    set_up_CP(cp, n_nodes, n_edges, nObs, observation, Eu, Ev
+             ,edgeWeight, nodeWeight);
+    cp->parameter.reg_strenth = lambda;
+    cp->parameter.cutoff = cutoff;
+    //-------run the optimization------------------------------------------
+    cp->run();
+    //------------write the solution-----------------------------
+    VertexAttributeMap<T> vertex_attribute_map = boost::get(
+            boost::vertex_bundle, cp->main_graph);
+    uint32_t ind_sol = 0;	
+    VertexIterator<T> ite_nod = boost::vertices(cp->main_graph).first;
+    for(uint32_t ind_nod = 0; ind_nod < n_nodes; ind_nod++ )
+    {        
+     
