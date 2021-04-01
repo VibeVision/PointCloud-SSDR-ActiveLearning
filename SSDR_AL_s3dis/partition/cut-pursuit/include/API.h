fee1e2fcb6d81ed3b62a2869b74251659b9f543b
VibeVision <VibeVisio80@proton.me>
2021-04-01 13:35:58 +0000

    Added content.

--- SSDR_AL_s3dis/partition/cut-pursuit/include/API.h
+++ SSDR_AL_s3dis/partition/cut-pursuit/include/API.h
@@ -448,4 +448,48 @@ void cut_pursuit(const uint32_t n_nodes, const uint32_t n_edges, const uint32_t 
     set_up_CP(cp, n_nodes, n_edges, nObs, observation, Eu, Ev
              ,edgeWeight, nodeWeight);
     cp->parameter.reg_strenth = lambda;
-	cp->para
+	cp->parameter.cutoff = cutoff;
+    //-------run the optimization------------------------------------------
+    cp->run();
+    //------------write the solution-----------------------------
+    VertexAttributeMap<T> vertex_attribute_map = boost::get(
+            boost::vertex_bundle, cp->main_graph);
+    VertexIterator<T> ite_nod = boost::vertices(cp->main_graph).first;
+    for(uint32_t ind_nod = 0; ind_nod < n_nodes; ind_nod++ )
+    {        
+	solution[ind_nod] = vertex_attribute_map[*ite_nod].value[0];
+        ite_nod++;
+    }
+    delete cp;
+    return;
+}
+
+//===========================================================================
+//=====================  cut_pursuit segmentation C++-style 23  ================
+//===========================================================================
+template<typename T>
+void cut_pursuit(const uint32_t n_nodes, const uint32_t n_edges, const uint32_t nObs
+          , std::vector< std::vector<T> > & observation
+          , const std::vector<uint32_t> & Eu, const std::vector<uint32_t> & Ev
+          , const std::vector<T> & edgeWeight, const std::vector<T> & nodeWeight
+          , std::vector< std::vector<T> > & solution
+	  , std::vector<uint32_t> & in_component, std::vector< std::vector<uint32_t> > & components
+          , uint32_t & n_nodes_red, uint32_t & n_edges_red
+          , std::vector<uint32_t> & Eu_red, std::vector<uint32_t> & Ev_red
+          , std::vector<T> & edgeWeight_red, std::vector<T> & nodeWeight_red
+  	  , const T lambda, const uint32_t cutoff, const T mode, const T speed, const T weight_decay
+          , const float verbose)
+{   //C-style ++ interface
+    std::srand (1);
+    if (verbose > 0)
+    {
+        std::cout << "L0-CUT PURSUIT";
+    }
+    //--------parameterization---------------------------------------------
+    CP::CutPursuit<T> * cp = create_CP(mode, verbose);
+
+    set_speed(cp, speed, weight_decay, verbose);
+    set_up_CP(cp, n_nodes, n_edges, nObs, observation, Eu, Ev
+             ,edgeWeight, nodeWeight);
+    cp->parameter.reg_strenth = lambda;
+	cp->paramete
