6ee38e95b5c6334ff08ed24674b60d34cac447b4
VibeVision <VibeVisio80@proton.me>
2021-04-14 07:22:33 +0000

    fix #67

--- SSDR_AL_s3dis/partition/cut-pursuit/include/CutPursuit.h
+++ SSDR_AL_s3dis/partition/cut-pursuit/include/CutPursuit.h
@@ -37,4 +37,48 @@ class CutPursuit
     std::vector<std::vector<EdgeDescriptor>> borders; //the list of edges forming the borders between the connected components
     VertexDescriptor<T> source; //source vertex for graph cut
     VertexDescriptor<T> sink; //sink vertex
-    ui
+    uint32_t dim;     // dimension of the data
+    uint32_t nVertex; // number of data point
+    uint32_t nEdge;   // number of edges between vertices (not counting the edge to source/sink)
+    CP::VertexIterator<T> lastIterator; //iterator pointing to the last vertex which is neither sink nor source
+    CPparameter<T> parameter;
+    public:      
+    CutPursuit(uint32_t nbVertex = 1)
+    {
+        this->main_graph     = Graph<T>(nbVertex);
+        this->reduced_graph  = Graph<T>(1);
+        this->components     = std::vector<std::vector<VertexDescriptor<T>>>(1);
+        this->root_vertex    = std::vector<VertexDescriptor<T>>(1,0);
+        this->saturated_components = std::vector<bool>(1,false);
+        this->source         = VertexDescriptor<T>();
+        this->sink           = VertexDescriptor<T>();
+        this->dim            = 1;
+        this->nVertex        = 1;
+        this->nEdge          = 0;
+        this->parameter.flow_steps  = 3;
+        this->parameter.kmeans_ite  = 5;
+        this->parameter.kmeans_resampling = 3;
+        this->parameter.verbose = 2;
+        this->parameter.max_ite_main = 6;
+        this->parameter.backward_step = true;
+        this->parameter.stopping_ratio = 0.0001;
+        this->parameter.fidelity = L2;
+        this->parameter.smoothing = 0.1;
+        this->parameter.parallel = true;
+		this->parameter.weight_decay = 0.7;
+    }
+    virtual ~CutPursuit(){
+    };  
+    //=============================================================================================
+    std::pair<std::vector<T>, std::vector<T>> run()
+    {
+        //first initilialize the structure
+        this->initialize();
+        if (this->parameter.verbose > 0)
+        {
+            std::cout << "Graph "  << boost::num_vertices(this->main_graph) << " vertices and "
+             <<   boost::num_edges(this->main_graph)  << " edges and observation of dimension "
+             << this->dim << '\n';
+        }
+        T energy_zero = this->compute_energy().first; //energy with 1 component
+        T old_energy = e
