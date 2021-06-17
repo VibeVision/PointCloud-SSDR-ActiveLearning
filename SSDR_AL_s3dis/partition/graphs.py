f95799699029168af6573e3692141756ce8f8f41
VibeVision <VibeVisio80@proton.me>
2021-06-17 14:34:51 +0000

    [taskcat-ci] Updating submodules

--- SSDR_AL_s3dis/partition/graphs.py
+++ SSDR_AL_s3dis/partition/graphs.py
@@ -14,4 +14,46 @@ def compute_graph_nn(xyz, k_nn):
     neighbors = neighbors[:, 1:]
     distances = distances[:, 1:]
     source = np.matlib.repmat(range(0, num_ver), k_nn, 1).flatten(order='F')
-    #
+    #save the graph
+    graph["source"] = source.flatten().astype('uint32')
+    graph["target"] = neighbors.flatten().astype('uint32')
+    graph["distances"] = distances.flatten().astype('float32')
+    return graph
+#------------------------------------------------------------------------------
+def compute_graph_nn_2(xyz, k_nn1, k_nn2, voronoi = 0.0):
+    """compute simultaneously 2 knn structures
+    only saves target for knn2
+    assumption : knn1 <= knn2"""
+    assert k_nn1 <= k_nn2, "knn1 must be smaller than knn2"
+    n_ver = xyz.shape[0]
+    #compute nearest neighbors
+    graph = dict([("is_nn", True)])
+    nn = NearestNeighbors(n_neighbors=k_nn2+1, algorithm='kd_tree').fit(xyz)
+    distances, neighbors = nn.kneighbors(xyz)
+    del nn
+    neighbors = neighbors[:, 1:]
+    distances = distances[:, 1:]
+    #---knn2---
+    target2 = (neighbors.flatten()).astype('uint32')
+    #---knn1-----
+    if voronoi>0:
+        tri = Delaunay(xyz)
+        graph["source"] = np.hstack((tri.vertices[:,0],tri.vertices[:,0], \
+              tri.vertices[:,0], tri.vertices[:,1], tri.vertices[:,1], tri.vertices[:,2])).astype('uint64')
+        graph["target"]= np.hstack((tri.vertices[:,1],tri.vertices[:,2], \
+              tri.vertices[:,3], tri.vertices[:,2], tri.vertices[:,3], tri.vertices[:,3])).astype('uint64')
+        graph["distances"] = ((xyz[graph["source"],:] - xyz[graph["target"],:])**2).sum(1)
+        keep_edges = graph["distances"]<voronoi
+        graph["source"] = graph["source"][keep_edges]
+        graph["target"] = graph["target"][keep_edges]
+        
+        graph["source"] = np.hstack((graph["source"], np.matlib.repmat(range(0, n_ver)
+            , k_nn1, 1).flatten(order='F').astype('uint32')))
+        neighbors = neighbors[:, :k_nn1]
+        graph["target"] =  np.hstack((graph["target"],np.transpose(neighbors.flatten(order='C')).astype('uint32')))
+        
+        edg_id = graph["source"] + n_ver * graph["target"]
+        
+        dump, unique_edges = np.unique(edg_id, return_index = True)
+        graph["source"] = graph["source"][unique_edges]
+        graph["target"] = graph["target"][unique_
