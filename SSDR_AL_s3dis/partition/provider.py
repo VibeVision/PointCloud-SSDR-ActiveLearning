489fa3575d1ef01bc6f413c310eddd5b185b7345
VibeVision <VibeVisio80@proton.me>
2021-07-05 20:36:32 +0000

    Add error handler

--- SSDR_AL_s3dis/partition/provider.py
+++ SSDR_AL_s3dis/partition/provider.py
@@ -44,4 +44,45 @@ def partition2ply(filename, xyz, components):
 #------------------------------------------------------------------------------
 def geof2ply(filename, xyz, geof):
     """write a ply with colors corresponding to geometric features"""
-    color = np.array(255 * geof[:, [0, 1, 3]
+    color = np.array(255 * geof[:, [0, 1, 3]], dtype='uint8')
+    prop = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]
+    vertex_all = np.empty(len(xyz), dtype=prop)
+    for i in range(0, 3):
+        vertex_all[prop[i][0]] = xyz[:, i]
+    for i in range(0, 3):
+        vertex_all[prop[i+3][0]] = color[:, i]
+    ply = PlyData([PlyElement.describe(vertex_all, 'vertex')], text=True)
+    ply.write(filename)
+#------------------------------------------------------------------------------
+def prediction2ply(filename, xyz, prediction, n_label, dataset):
+    """write a ply with colors for each class"""
+    if len(prediction.shape) > 1 and prediction.shape[1] > 1:
+        prediction = np.argmax(prediction, axis = 1)
+    color = np.zeros(xyz.shape)
+    for i_label in range(0, n_label + 1):
+        color[np.where(prediction == i_label), :] = get_color_from_label(i_label, dataset)
+    prop = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]
+    vertex_all = np.empty(len(xyz), dtype=prop)
+    for i in range(0, 3):
+        vertex_all[prop[i][0]] = xyz[:, i]
+    for i in range(0, 3):
+        vertex_all[prop[i+3][0]] = color[:, i]
+    ply = PlyData([PlyElement.describe(vertex_all, 'vertex')], text=True)
+    ply.write(filename)
+#------------------------------------------------------------------------------
+def error2ply(filename, xyz, rgb, labels, prediction):
+    """write a ply with green hue for correct classifcation and red for error"""
+    if len(prediction.shape) > 1 and prediction.shape[1] > 1:
+        prediction = np.argmax(prediction, axis = 1)
+    if len(labels.shape) > 1 and labels.shape[1] > 1:
+        labels = np.argmax(labels, axis = 1)
+    color_rgb = rgb/255
+    for i_ver in range(0, len(labels)):
+
+        color_hsv = list(colorsys.rgb_to_hsv(color_rgb[i_ver,0], color_rgb[i_ver,1], color_rgb[i_ver,2]))
+        if (labels[i_ver] == prediction[i_ver]) or (labels[i_ver]==0):
+            color_hsv[0] = 0.333333
+        else:
+            color_hsv[0] = 0
+        color_hsv[1] = min(1, color_hsv[1] + 0.3)
+        color_hsv
