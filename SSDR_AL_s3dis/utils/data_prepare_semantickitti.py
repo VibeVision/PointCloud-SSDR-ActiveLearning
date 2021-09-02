f6c0e9ff521fdef4fd865444ac47de43e450a949
VibeVision <VibeVisio80@proton.me>
2021-09-02 11:30:58 +0000

    powerpc/MSI: Fix race condition in tearing down MSI interrupts

---
+++ SSDR_AL_s3dis/utils/data_prepare_semantickitti.py
@@ -0,0 +1,40 @@
+import pickle, yaml, os, sys
+import numpy as np
+from os.path import join, exists, dirname, abspath
+from sklearn.neighbors import KDTree
+
+BASE_DIR = dirname(abspath(__file__))
+ROOT_DIR = dirname(BASE_DIR)
+sys.path.append(BASE_DIR)
+sys.path.append(ROOT_DIR)
+from helper_tool import DataProcessing as DP
+
+data_config = os.path.join(BASE_DIR, 'semantic-kitti.yaml')
+DATA = yaml.safe_load(open(data_config, 'r'))
+remap_dict = DATA["learning_map"]
+max_key = max(remap_dict.keys())
+remap_lut = np.zeros((max_key + 100), dtype=np.int32)
+remap_lut[list(remap_dict.keys())] = list(remap_dict.values())
+
+grid_size = 0.06
+# dataset_path = './data/semantic_kitti/dataset/sequences'
+# output_path = './data/semantic_kitti/dataset/sequences' + '_' + str(grid_size)
+dataset_path = './data/SemanticKITTI/dataset/sequences'
+output_path = './data/SemanticKITTI/dataset/sequences' + '_' + str(grid_size)
+seq_list = np.sort(os.listdir(dataset_path))
+
+for seq_id in seq_list:
+    print('sequence' + seq_id + ' start')
+    seq_path = join(dataset_path, seq_id)
+    seq_path_out = join(output_path, seq_id)
+    pc_path = join(seq_path, 'velodyne')
+    pc_path_out = join(seq_path_out, 'velodyne')
+    KDTree_path_out = join(seq_path_out, 'KDTree')
+    os.makedirs(seq_path_out) if not exists(seq_path_out) else None
+    os.makedirs(pc_path_out) if not exists(pc_path_out) else None
+    os.makedirs(KDTree_path_out) if not exists(KDTree_path_out) else None
+
+    if int(seq_id) < 11:
+        label_path = join(seq_path, 'labels')
+        label_path_out = join(seq_path_out, 'labels')
+        os.makedirs(label_path_out) if not exists(label_path_out) 
