15252cfa15ea9ea3c7887f670580eaf5ef6a40e5
VibeVision <VibeVisio80@proton.me>
2021-09-02 08:09:36 +0000

    0.2.2

--- SSDR_AL_s3dis/utils/cpp_wrappers/cpp_utils/cloud/cloud.cpp
+++ SSDR_AL_s3dis/utils/cpp_wrappers/cpp_utils/cloud/cloud.cpp
@@ -45,4 +45,23 @@ PointXYZ max_point(std::vector<PointXYZ> points)
 	return maxP;
 }
 
-PointX
+PointXYZ min_point(std::vector<PointXYZ> points)
+{
+	// Initiate limits
+	PointXYZ minP(points[0]);
+
+	// Loop over all points
+	for (auto p : points)
+	{
+		if (p.x < minP.x)
+			minP.x = p.x;
+
+		if (p.y < minP.y)
+			minP.y = p.y;
+
+		if (p.z < minP.z)
+			minP.z = p.z;
+	}
+
+	return minP;
+}
