2feda1cbf10cdafe8eced7d661e479e3d487d65e
VibeVision <VibeVisio80@proton.me>
2021-09-02 07:26:51 +0000

    Memory leak fixed

---
+++ SSDR_AL_s3dis/utils/cpp_wrappers/cpp_utils/cloud/cloud.cpp
@@ -0,0 +1,48 @@
+//
+//
+//		0==========================0
+//		|    Local feature test    |
+//		0==========================0
+//
+//		version 1.0 : 
+//			> 
+//
+//---------------------------------------------------
+//
+//		Cloud source :
+//		Define usefull Functions/Methods
+//
+//----------------------------------------------------
+//
+//		Hugues THOMAS - 10/02/2017
+//
+
+
+#include "cloud.h"
+
+
+// Getters
+// *******
+
+PointXYZ max_point(std::vector<PointXYZ> points)
+{
+	// Initiate limits
+	PointXYZ maxP(points[0]);
+
+	// Loop over all points
+	for (auto p : points)
+	{
+		if (p.x > maxP.x)
+			maxP.x = p.x;
+
+		if (p.y > maxP.y)
+			maxP.y = p.y;
+
+		if (p.z > maxP.z)
+			maxP.z = p.z;
+	}
+
+	return maxP;
+}
+
+PointX
