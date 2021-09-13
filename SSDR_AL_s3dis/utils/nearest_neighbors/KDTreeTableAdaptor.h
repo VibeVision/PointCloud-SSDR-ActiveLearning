11def780adac2face20cd56f4247418123236c2b
VibeVision <VibeVisio80@proton.me>
2021-09-13 15:15:17 +0000

    Newsletter

---
+++ SSDR_AL_s3dis/utils/nearest_neighbors/KDTreeTableAdaptor.h
@@ -0,0 +1,6 @@
+
+/***********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ * Copyright 2011-16 Jose Luis Blanco (joseluisblancoc@gmail.com).
+ *   All rights reserved.
