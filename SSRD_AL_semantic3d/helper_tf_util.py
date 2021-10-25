478f863f266cb0a6067ed71154111e92e677ac50
VibeVision <VibeVisio80@proton.me>
2021-10-25 18:22:06 +0000

    pep8ify

--- SSRD_AL_semantic3d/helper_tf_util.py
+++ SSRD_AL_semantic3d/helper_tf_util.py
@@ -319,3 +319,255 @@ def fully_connected(inputs,
     """ Fully connected layer with non-linear operation.
 
     Args:
+      inputs: 2-D tensor BxN
+      num_outputs: int
+
+    Returns:
+      Variable tensor of size B x num_outputs.
+    """
+    with tf.variable_scope(scope) as sc:
+        num_input_units = inputs.get_shape()[-1].value
+        weights = _variable_with_weight_decay('weights',
+                                              shape=[num_input_units, num_outputs],
+                                              use_xavier=use_xavier,
+                                              stddev=stddev,
+                                              wd=weight_decay)
+        outputs = tf.matmul(inputs, weights)
+        biases = _variable_on_cpu('biases', [num_outputs],
+                                  tf.constant_initializer(0.0))
+        outputs = tf.nn.bias_add(outputs, biases)
+
+        if bn:
+            outputs = batch_norm_for_fc(outputs, is_training, bn_decay, 'bn')
+
+        if activation_fn is not None:
+            # outputs = activation_fn(outputs)
+            outputs = tf.nn.leaky_relu(outputs, alpha=0.2)
+        return outputs
+
+
+def max_pool2d(inputs,
+               kernel_size,
+               scope,
+               stride=[2, 2],
+               padding='VALID'):
+    """ 2D max pooling.
+
+    Args:
+      inputs: 4-D tensor BxHxWxC
+      kernel_size: a list of 2 ints
+      stride: a list of 2 ints
+
+    Returns:
+      Variable tensor
+    """
+    with tf.variable_scope(scope) as sc:
+        kernel_h, kernel_w = kernel_size
+        stride_h, stride_w = stride
+        outputs = tf.nn.max_pool(inputs,
+                                 ksize=[1, kernel_h, kernel_w, 1],
+                                 strides=[1, stride_h, stride_w, 1],
+                                 padding=padding,
+                                 name=sc.name)
+        return outputs
+
+
+def avg_pool2d(inputs,
+               kernel_size,
+               scope,
+               stride=[2, 2],
+               padding='VALID'):
+    """ 2D avg pooling.
+
+    Args:
+      inputs: 4-D tensor BxHxWxC
+      kernel_size: a list of 2 ints
+      stride: a list of 2 ints
+
+    Returns:
+      Variable tensor
+    """
+    with tf.variable_scope(scope) as sc:
+        kernel_h, kernel_w = kernel_size
+        stride_h, stride_w = stride
+        outputs = tf.nn.avg_pool(inputs,
+                                 ksize=[1, kernel_h, kernel_w, 1],
+                                 strides=[1, stride_h, stride_w, 1],
+                                 padding=padding,
+                                 name=sc.name)
+        return outputs
+
+
+def max_pool3d(inputs,
+               kernel_size,
+               scope,
+               stride=[2, 2, 2],
+               padding='VALID'):
+    """ 3D max pooling.
+
+    Args:
+      inputs: 5-D tensor BxDxHxWxC
+      kernel_size: a list of 3 ints
+      stride: a list of 3 ints
+
+    Returns:
+      Variable tensor
+    """
+    with tf.variable_scope(scope) as sc:
+        kernel_d, kernel_h, kernel_w = kernel_size
+        stride_d, stride_h, stride_w = stride
+        outputs = tf.nn.max_pool3d(inputs,
+                                   ksize=[1, kernel_d, kernel_h, kernel_w, 1],
+                                   strides=[1, stride_d, stride_h, stride_w, 1],
+                                   padding=padding,
+                                   name=sc.name)
+        return outputs
+
+
+def avg_pool3d(inputs,
+               kernel_size,
+               scope,
+               stride=[2, 2, 2],
+               padding='VALID'):
+    """ 3D avg pooling.
+
+    Args:
+      inputs: 5-D tensor BxDxHxWxC
+      kernel_size: a list of 3 ints
+      stride: a list of 3 ints
+
+    Returns:
+      Variable tensor
+    """
+    with tf.variable_scope(scope) as sc:
+        kernel_d, kernel_h, kernel_w = kernel_size
+        stride_d, stride_h, stride_w = stride
+        outputs = tf.nn.avg_pool3d(inputs,
+                                   ksize=[1, kernel_d, kernel_h, kernel_w, 1],
+                                   strides=[1, stride_d, stride_h, stride_w, 1],
+                                   padding=padding,
+                                   name=sc.name)
+        return outputs
+
+
+def batch_norm_template(inputs, is_training, scope, moments_dims, bn_decay):
+    """ Batch normalization on convolutional maps and beyond...
+
+
+    Args:
+        inputs:        Tensor, k-D input ... x C could be BC or BHWC or BDHWC
+        is_training:   boolean tf.Varialbe, true indicates training phase
+        scope:         string, variable scope
+        moments_dims:  a list of ints, indicating dimensions for moments calculation
+        bn_decay:      float or float tensor variable, controling moving average weight
+    Return:
+        normed:        batch-normalized maps
+    """
+    with tf.variable_scope(scope) as sc:
+        num_channels = inputs.get_shape()[-1].value
+        beta = tf.Variable(tf.constant(0.0, shape=[num_channels]),
+                           name='beta', trainable=True)
+        gamma = tf.Variable(tf.constant(1.0, shape=[num_channels]),
+                            name='gamma', trainable=True)
+        batch_mean, batch_var = tf.nn.moments(inputs, moments_dims, name='moments')
+        decay = bn_decay if bn_decay is not None else 0.9
+        ema = tf.train.ExponentialMovingAverage(decay=decay)
+        # Operator that maintains moving averages of variables.
+        ema_apply_op = tf.cond(is_training,
+                               lambda: ema.apply([batch_mean, batch_var]),
+                               lambda: tf.no_op())
+
+        # Update moving average and return current batch's avg and var.
+        def mean_var_with_update():
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+
+        # ema.average returns the Variable holding the average of var.
+        mean, var = tf.cond(is_training,
+                            mean_var_with_update,
+                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
+        normed = tf.nn.batch_normalization(inputs, mean, var, beta, gamma, 1e-3)
+    return normed
+
+
+def batch_norm_for_fc(inputs, is_training, bn_decay, scope):
+    """ Batch normalization on FC data.
+
+    Args:
+        inputs:      Tensor, 2D BxC input
+        is_training: boolean tf.Varialbe, true indicates training phase
+        bn_decay:    float or float tensor variable, controling moving average weight
+        scope:       string, variable scope
+    Return:
+        normed:      batch-normalized maps
+    """
+    return batch_norm_template(inputs, is_training, scope, [0, ], bn_decay)
+
+
+def batch_norm_for_conv1d(inputs, is_training, bn_decay, scope):
+    """ Batch normalization on 1D convolutional maps.
+
+    Args:
+        inputs:      Tensor, 3D BLC input maps
+        is_training: boolean tf.Varialbe, true indicates training phase
+        bn_decay:    float or float tensor variable, controling moving average weight
+        scope:       string, variable scope
+    Return:
+        normed:      batch-normalized maps
+    """
+    return batch_norm_template(inputs, is_training, scope, [0, 1], bn_decay)
+
+
+def batch_norm_for_conv2d(inputs, is_training, bn_decay, scope):
+    """ Batch normalization on 2D convolutional maps.
+
+    Args:
+        inputs:      Tensor, 4D BHWC input maps
+        is_training: boolean tf.Varialbe, true indicates training phase
+        bn_decay:    float or float tensor variable, controling moving average weight
+        scope:       string, variable scope
+    Return:
+        normed:      batch-normalized maps
+    """
+    return batch_norm_template(inputs, is_training, scope, [0, 1, 2], bn_decay)
+
+
+def batch_norm_for_conv3d(inputs, is_training, bn_decay, scope):
+    """ Batch normalization on 3D convolutional maps.
+
+    Args:
+        inputs:      Tensor, 5D BDHWC input maps
+        is_training: boolean tf.Varialbe, true indicates training phase
+        bn_decay:    float or float tensor variable, controling moving average weight
+        scope:       string, variable scope
+    Return:
+        normed:      batch-normalized maps
+    """
+    return batch_norm_template(inputs, is_training, scope, [0, 1, 2, 3], bn_decay)
+
+
+def dropout(inputs,
+            is_training,
+            scope,
+            keep_prob=0.5,
+            noise_shape=None):
+    """ Dropout layer.
+
+    Args:
+      inputs: tensor
+      is_training: boolean tf.Variable
+      scope: string
+      keep_prob: float in [0,1]
+      noise_shape: list of ints
+
+    Returns:
+      tensor variable
+    """
+    with tf.variable_scope(scope) as sc:
+        outputs = tf.cond(is_training,
+                          lambda: tf.nn.dropout(inputs, keep_prob, noise_shape),
+                          lambda: inputs)
+        return outputs
+
+# def path_from_input_to_superpoint(input_path, sub_grid_size):
+#     ssdr_path = input_path.replace('input_{:.3f}'.format(sub_grid_size), "superpoint")
