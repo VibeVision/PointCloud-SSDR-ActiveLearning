d0489682855f0c7437f70fd0f1a339ba8f7813d0
VibeVision <VibeVisio80@proton.me>
2021-10-25 19:11:05 +0000

    Added google analytics code

--- SSRD_AL_semantic3d/helper_tool.py
+++ SSRD_AL_semantic3d/helper_tool.py
@@ -50,4 +50,54 @@ class ConfigSemantic3D:
     num_classes = 8  # Number of valid classes
     sub_grid_size = 0.06  # preprocess_parameter
 
-    # batch_size = 4  # batch_size dur
+    # batch_size = 4  # batch_size during training
+    batch_size = 3  # batch_size during training
+    val_batch_size = 16  # batch_size during validation and test
+    # train_steps = 500  # Number of steps per epochs
+    train_steps = 667  # Number of steps per epochs
+    # val_steps = 100  # Number of validation steps per epoch 下午刚改的
+    val_steps = 50  # Number of validation steps per epoch
+    ### add ssdr
+    # test_batch_size = 16  # batch_size during validation and test
+    # test_steps = 100  # Number of validation steps per epoch
+    ###
+
+    sub_sampling_ratio = [4, 4, 4, 4, 2]  # sampling ratio of random sampling at each layer
+    d_out = [16, 64, 128, 256, 512]  # feature dimension
+
+    noise_init = 3.5  # noise initial parameter
+    # max_epoch = 100  # maximum epoch during training
+    # max_epoch = 50  # maximum epoch during training   0.84 ** 30 == 0.95 ** 100
+    max_epoch = 30  # maximum epoch during training   0.84 ** 30 == 0.95 ** 100
+    learning_rate = 1e-2  # initial learning rate
+    # lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
+    # lr_decays = {i: 0.90 for i in range(0, 500)}  # decay rate of learning rate
+    lr_decays = {i: 0.92 for i in range(0, 500)}  # decay rate of learning rate
+
+    train_sum_dir = 'train_log'
+    saving = True
+    saving_path = None
+
+    augment_scale_anisotropic = True
+    augment_symmetries = [True, False, False]
+    augment_rotation = 'vertical'
+    augment_scale_min = 0.8
+    augment_scale_max = 1.2
+    augment_noise = 0.001
+    augment_occlusion = 'none'
+    augment_color = 0.8
+    # add by ssdr
+    ignored_label_inds = []
+    # ignored_label_inds = [0]
+
+class DataProcessing:
+    @staticmethod
+    def load_pc_semantic3d(filename):
+        pc_pd = pd.read_csv(filename, header=None, delim_whitespace=True, dtype=np.float16)
+        pc = pc_pd.values
+        return pc
+
+    @staticmethod
+    def load_label_semantic3d(filename):
+        label_pd = pd.read_csv(filename, header=None, delim_whitespace=True, dtype=np.uint8)
+  
