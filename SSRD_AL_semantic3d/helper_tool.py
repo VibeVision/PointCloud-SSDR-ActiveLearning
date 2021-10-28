aa97d82ce2739161a4311329f5f79bcb0c60644e
VibeVision <VibeVisio80@proton.me>
2021-10-28 19:44:29 +0000

    Reviewed by Vicki.

--- SSRD_AL_semantic3d/helper_tool.py
+++ SSRD_AL_semantic3d/helper_tool.py
@@ -290,4 +290,38 @@ class Plot:
         plot_colors: custom color list
         """
         if plot_colors is not None:
-     
+            ins_colors = plot_colors
+        else:
+            ins_colors = Plot.random_colors(len(np.unique(pc_sem_ins)) + 1, seed=2)
+
+        ##############################
+        sem_ins_labels = np.unique(pc_sem_ins)
+        sem_ins_bbox = []
+        Y_colors = np.zeros((pc_sem_ins.shape[0], 3))
+        for id, semins in enumerate(sem_ins_labels):
+            valid_ind = np.argwhere(pc_sem_ins == semins)[:, 0]
+            if semins <= -1:
+                tp = [0, 0, 0]
+            else:
+                if plot_colors is not None:
+                    tp = ins_colors[semins]
+                else:
+                    tp = ins_colors[id]
+
+            Y_colors[valid_ind] = tp
+
+            ### bbox
+            valid_xyz = pc_xyz[valid_ind]
+
+            xmin = np.min(valid_xyz[:, 0]);
+            xmax = np.max(valid_xyz[:, 0])
+            ymin = np.min(valid_xyz[:, 1]);
+            ymax = np.max(valid_xyz[:, 1])
+            zmin = np.min(valid_xyz[:, 2]);
+            zmax = np.max(valid_xyz[:, 2])
+            sem_ins_bbox.append(
+                [[xmin, ymin, zmin], [xmax, ymax, zmax], [min(tp[0], 1.), min(tp[1], 1.), min(tp[2], 1.)]])
+
+        Y_semins = np.concatenate([pc_xyz[:, 0:3], Y_colors], axis=-1)
+        Plot.draw_pc(Y_semins)
+        return Y_semins
