ebc1199c2f73472ea97b3f6ab9b9892c2dbeeaec
VibeVision <VibeVisio80@proton.me>
2021-12-19 22:45:57 +0000

    Bump version to 0.13.1

--- SSRD_AL_semantic3d/partition/cut-pursuit/include/CutPursuit_Linear.h
+++ SSRD_AL_semantic3d/partition/cut-pursuit/include/CutPursuit_Linear.h
@@ -38,3 +38,45 @@ class CutPursuit_Linear : public CutPursuit<T>
                         * vertex_attribute_map(*i_ver).value[i_dim];
             }
         }
+        pair_energy.first = energy;
+        energy = 0;
+        EdgeIterator<T> i_edg, i_edg_end =  boost::edges(this->main_graph).second;
+        for (i_edg = boost::edges(this->main_graph).first;
+             i_edg != i_edg_end; ++i_edg)
+        {
+            if (!edge_attribute_map(*i_edg).realEdge)
+            {
+                continue;
+            }
+            energy += .5 * edge_attribute_map(*i_edg).isActive * this->parameter.reg_strenth
+                    * edge_attribute_map(*i_edg).weight;
+        }
+        pair_energy.second = energy;
+        return pair_energy;
+    }
+
+    //=============================================================================================
+    //=============================        SPLIT        ===========================================
+    //=============================================================================================
+    virtual uint32_t split()
+    { // split the graph by trying to find the best binary partition
+      // each components is split into B and notB
+        uint32_t saturation;
+        //initialize h_1 and h_2 with kmeans
+        //--------initilializing labels------------------------------------------------------------
+        //corner contains the two most likely class for each component
+        std::vector< std::vector< uint32_t > > corners =
+                std::vector< std::vector< uint32_t > >(this->components.size(),
+                std::vector< uint32_t >(2,0));
+        this->compute_corners(corners);
+        this->set_capacities(corners);
+        //compute flow
+        boost::boykov_kolmogorov_max_flow(
+                   this->main_graph,
+                   get(&EdgeAttribute<T>::capacity        , this->main_graph),
+                   get(&EdgeAttribute<T>::residualCapacity, this->main_graph),
+                   get(&EdgeAttribute<T>::edge_reverse     , this->main_graph),
+                   get(&VertexAttribute<T>::color         , this->main_graph),
+                   get(boost::vertex_index                , this->main_graph),
+                   this->source,
+                   
