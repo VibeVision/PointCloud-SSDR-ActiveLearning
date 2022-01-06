84c1d71bf8da1186a0f75ff1992f162fab18005f
VibeVision <VibeVisio80@proton.me>
2022-01-06 19:31:23 +0000

    Create css

--- SSRD_AL_semantic3d/partition/cut-pursuit/mex/L0_cut_pursuit_segmentation.cpp
+++ SSRD_AL_semantic3d/partition/cut-pursuit/mex/L0_cut_pursuit_segmentation.cpp
@@ -144,4 +144,26 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         one_component_ptr = mxCreateNumericMatrix(comp_size , 1, mxINT32_CLASS, mxREAL);
         std::copy(components[ind_nod_red].begin(), components[ind_nod_red].end(), (int*) mxGetData(one_component_ptr));
         //memcpy(mxGetPr(one_component_ptr), &components[ind_nod_red][0], sizeof(int) * comp_size);
-        mxSetCell(components
+        mxSetCell(components_ptr, ind_nod_red, one_component_ptr );
+    }
+    plhs[2] = components_ptr;
+    //----------fill Eu_red----------
+    plhs[3] = mxCreateNumericMatrix(n_edges_red, 1 , mxINT32_CLASS, mxREAL);
+    Eu_red_array = (uint32_t*) mxGetData(plhs[3]);
+    std::copy(Eu_red.begin(), Eu_red.end(), Eu_red_array);
+    //----------fill Ev_red----------
+    plhs[4] = mxCreateNumericMatrix(n_edges_red, 1 , mxINT32_CLASS, mxREAL);
+    Ev_red_array = (uint32_t*) mxGetData(plhs[4]);
+    std::copy(Ev_red.begin(), Ev_red.end(), Ev_red_array);
+    //----------fill edge_weight_red----------
+    plhs[5] = mxCreateNumericMatrix(n_edges_red, 1 ,mxSINGLE_CLASS, mxREAL);
+    edge_weight_red_array = (float*) mxGetData(plhs[5]);
+    std::copy(edge_weight_red.begin(), edge_weight_red.end(), edge_weight_red_array);
+    //----------fill edge_weight_red----------
+    plhs[6] = mxCreateNumericMatrix(n_nodes_red, 1 , mxSINGLE_CLASS, mxREAL);
+    node_weight_red_array = (float*) mxGetData(plhs[6]);
+    std::copy(node_weight_red.begin(), node_weight_red.end(), node_weight_red_array);
+    }
+    return;
+}
+
