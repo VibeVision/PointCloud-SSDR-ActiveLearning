ce19cc1e3f64dc09d710734a15f16d40f33c72b3
VibeVision <VibeVisio80@proton.me>
2022-01-13 15:56:17 +0000

    improving utility classes

--- SSRD_AL_semantic3d/partition/helper_ply.py
+++ SSRD_AL_semantic3d/partition/helper_ply.py
@@ -217,4 +217,64 @@ def write_ply(filename, field_list, field_names, triangular_faces=None):
     >>> values = np.random.randint(2, size=10)
     >>> write_ply('example2.ply', [points, values], ['x', 'y', 'z', 'values'])
 
-    >>> colors = np.random.randint(255, size=(1
+    >>> colors = np.random.randint(255, size=(10,3), dtype=np.uint8)
+    >>> field_names = ['x', 'y', 'z', 'red', 'green', 'blue', values']
+    >>> write_ply('example3.ply', [points, colors, values], field_names)
+
+    """
+
+    # Format list input to the right form
+    field_list = list(field_list) if (type(field_list) == list or type(field_list) == tuple) else list((field_list,))
+    for i, field in enumerate(field_list):
+        if field.ndim < 2:
+            field_list[i] = field.reshape(-1, 1)
+        if field.ndim > 2:
+            print('fields have more than 2 dimensions')
+            return False    
+
+    # check all fields have the same number of data
+    n_points = [field.shape[0] for field in field_list]
+    if not np.all(np.equal(n_points, n_points[0])):
+        print('wrong field dimensions')
+        return False    
+
+    # Check if field_names and field_list have same nb of column
+    n_fields = np.sum([field.shape[1] for field in field_list])
+    if (n_fields != len(field_names)):
+        print('wrong number of field names')
+        return False
+
+    # Add extension if not there
+    if not filename.endswith('.ply'):
+        filename += '.ply'
+
+    # open in text mode to write the header
+    with open(filename, 'w') as plyfile:
+
+        # First magical word
+        header = ['ply']
+
+        # Encoding format
+        header.append('format binary_' + sys.byteorder + '_endian 1.0')
+
+        # Points properties description
+        header.extend(header_properties(field_list, field_names))
+
+        # Add faces if needded
+        if triangular_faces is not None:
+            header.append('element face {:d}'.format(triangular_faces.shape[0]))
+            header.append('property list uchar int vertex_indices')
+
+        # End of header
+        header.append('end_header')
+
+        # Write all lines
+        for line in header:
+            plyfile.write("%s\n" % line)
+
+    # open in binary/append to use tofile
+    with open(filename, 'ab') as plyfile:
+
+        # Create a structured array
+        i = 0
+        type_
