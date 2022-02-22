4832c29bf54ea6824a00a5bccad47a47135a9664
VibeVision <VibeVisio80@proton.me>
2022-02-22 19:29:21 +0000

    Change method signature

--- SSRD_AL_semantic3d/partition/provider.py
+++ SSRD_AL_semantic3d/partition/provider.py
@@ -107,4 +107,43 @@ def spg2ply(filename, spg_graph):
     edges_val = np.empty((spg_graph['source']).shape[0], dtype=edges_prop)
     edges_val[edges_prop[0][0]] = spg_graph['source'].flatten()
     edges_val[edges_prop[1][0]] = spg_graph['target'].flatten()
-    ply =
+    ply = PlyData([PlyElement.describe(vertex_val, 'vertex'), PlyElement.describe(edges_val, 'edge')], text=True)
+    ply.write(filename)
+#------------------------------------------------------------------------------
+def scalar2ply(filename, xyz, scalar):
+    """write a ply with an unisgned integer scalar field"""
+    prop = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('scalar', 'f4')]
+    vertex_all = np.empty(len(xyz), dtype=prop)
+    for i in range(0, 3):
+        vertex_all[prop[i][0]] = xyz[:, i]
+    vertex_all[prop[3][0]] = scalar
+    ply = PlyData([PlyElement.describe(vertex_all, 'vertex')], text=True)
+
+    ply.write(filename)
+#------------------------------------------------------------------------------
+def get_color_from_label(object_label, dataset):
+    """associate the color corresponding to the class"""
+    if dataset == 's3dis': #S3DIS
+        object_label = {
+            0: [0   ,   0,   0], #unlabelled .->. black
+            1: [ 233, 229, 107], #'ceiling' .-> .yellow
+            2: [  95, 156, 196], #'floor' .-> . blue
+            3: [ 179, 116,  81], #'wall'  ->  brown
+            4: [  81, 163, 148], #'column'  ->  bluegreen
+            5: [ 241, 149, 131], #'beam'  ->  salmon
+            6: [  77, 174,  84], #'window'  ->  bright green
+            7: [ 108, 135,  75], #'door'   ->  dark green
+            8: [  79,  79,  76], #'table'  ->  dark grey
+            9: [  41,  49, 101], #'chair'  ->  darkblue
+            10: [223,  52,  52], #'bookcase'  ->  red
+            11: [ 89,  47,  95], #'sofa'  ->  purple
+            12: [ 81, 109, 114], #'board'   ->  grey
+            13: [233, 233, 229], #'clutter'  ->  light grey
+            }.get(object_label, -1)
+    elif (dataset == 'sema3d'): #Semantic3D
+        object_label =  {
+            0: [0   ,   0,   0], #unlabelled .->. black
+            1: [ 200, 200, 200], #'man-made terrain'  ->  grey
+            2: [   0,  70,   0], #'natural terrain'  ->  dark green
+            3: [   0, 255,   0], #'high vegetation'  ->  bright green
+            4: [ 255, 255,   0], #'low
