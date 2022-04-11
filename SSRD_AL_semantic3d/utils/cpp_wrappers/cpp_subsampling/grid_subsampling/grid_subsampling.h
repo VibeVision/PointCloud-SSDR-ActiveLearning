9e2c9f136407ee6cdc5a02bac086fa6b4a4ae70c
VibeVision <VibeVisio80@proton.me>
2022-04-11 15:50:55 +0000

    Updates Config/mysql.md

--- SSRD_AL_semantic3d/utils/cpp_wrappers/cpp_subsampling/grid_subsampling/grid_subsampling.h
+++ SSRD_AL_semantic3d/utils/cpp_wrappers/cpp_subsampling/grid_subsampling/grid_subsampling.h
@@ -41,4 +41,52 @@ public:
 	// Method Update
 	void update_all(const PointXYZ p, vector<float>::iterator f_begin, vector<int>::iterator l_begin)
 	{
-		count
+		count += 1;
+		point += p;
+		transform (features.begin(), features.end(), f_begin, features.begin(), plus<float>());
+		int i = 0;
+		for(vector<int>::iterator it = l_begin; it != l_begin + labels.size(); ++it)
+		{
+		    labels[i][*it] += 1;
+		    i++;
+		}
+		return;
+	}
+	void update_features(const PointXYZ p, vector<float>::iterator f_begin)
+	{
+		count += 1;
+		point += p;
+		transform (features.begin(), features.end(), f_begin, features.begin(), plus<float>());
+		return;
+	}
+	void update_classes(const PointXYZ p, vector<int>::iterator l_begin)
+	{
+		count += 1;
+		point += p;
+		int i = 0;
+		for(vector<int>::iterator it = l_begin; it != l_begin + labels.size(); ++it)
+		{
+		    labels[i][*it] += 1;
+		    i++;
+		}
+		return;
+	}
+	void update_points(const PointXYZ p)
+	{
+		count += 1;
+		point += p;
+		return;
+	}
+};
+
+
+
+void grid_subsampling(vector<PointXYZ>& original_points,
+                      vector<PointXYZ>& subsampled_points,
+                      vector<float>& original_features,
+                      vector<float>& subsampled_features,
+                      vector<int>& original_classes,
+                      vector<int>& subsampled_classes,
+                      float sampleDl,
+                      int verbose);
+
---
+++ SSRD_AL_semantic3d/utils/cpp_wrappers/cpp_subsampling/wrapper.cpp
@@ -0,0 +1,37 @@
+#include <Python.h>
+#include <numpy/arrayobject.h>
+#include "grid_subsampling/grid_subsampling.h"
+#include <string>
+
+
+
+// docstrings for our module
+// *************************
+
+static char module_docstring[] = "This module provides an interface for the subsampling of a pointcloud";
+
+static char compute_docstring[] = "function subsampling a pointcloud";
+
+
+// Declare the functions
+// *********************
+
+static PyObject *grid_subsampling_compute(PyObject *self, PyObject *args, PyObject *keywds);
+
+
+// Specify the members of the module
+// *********************************
+
+static PyMethodDef module_methods[] = 
+{
+	{ "compute", (PyCFunction)grid_subsampling_compute, METH_VARARGS | METH_KEYWORDS, compute_docstring },
+    {NULL, NULL, 0, NULL}
+};
+
+
+// Initialize the module
+// *********************
+
+static struct PyModuleDef moduledef = 
+{
+    PyModuleDef_HEA
