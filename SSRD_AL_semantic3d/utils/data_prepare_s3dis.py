93323a30b41ac224a1d30564f5baf7c11ca65c0e
VibeVision <VibeVisio80@proton.me>
2022-05-08 17:04:33 +0000

    More links.

--- SSRD_AL_semantic3d/utils/data_prepare_s3dis.py
+++ SSRD_AL_semantic3d/utils/data_prepare_s3dis.py
@@ -11,4 +11,49 @@ sys.path.append(ROOT_DIR)
 from helper_ply import write_ply
 from helper_tool import DataProcessing as DP
 
-dataset_path = './data/S3DIS/Stanf
+dataset_path = './data/S3DIS/Stanford3dDataset_v1.2_Aligned_Version'
+anno_paths = [line.rstrip() for line in open(join(BASE_DIR, 'meta/anno_paths.txt'))]
+anno_paths = [join(dataset_path, p) for p in anno_paths]
+
+gt_class = [x.rstrip() for x in open(join(BASE_DIR, 'meta/class_names.txt'))]
+gt_class2label = {cls: i for i, cls in enumerate(gt_class)}
+
+sub_grid_size = 0.04
+original_pc_folder = join(dirname(dataset_path), 'original_ply')
+sub_pc_folder = join(dirname(dataset_path), 'input_{:.3f}'.format(sub_grid_size))
+print(original_pc_folder)
+os.mkdir(original_pc_folder) if not exists(original_pc_folder) else None
+os.mkdir(sub_pc_folder) if not exists(sub_pc_folder) else None
+out_format = '.ply'
+
+
+def convert_pc2ply(anno_path, save_path):
+    """
+    Convert original dataset files to ply file (each line is XYZRGBL).
+    We aggregated all the points from each instance in the room.
+    :param anno_path: path to annotations. e.g. Area_1/office_2/Annotations/
+    :param save_path: path to save original point clouds (each line is XYZRGBL)
+    :return: None
+    """
+    data_list = []
+
+    for f in glob.glob(join(anno_path, '*.txt')):
+        class_name = os.path.basename(f).split('_')[0]
+        if class_name not in gt_class:  # note: in some room there is 'staris' class..
+            class_name = 'clutter'
+        pc = pd.read_csv(f, header=None, delim_whitespace=True).values
+        labels = np.ones((pc.shape[0], 1)) * gt_class2label[class_name]
+        data_list.append(np.concatenate([pc, labels], 1))  # Nx7, 7=xyz + rbg + label
+
+    pc_label = np.concatenate(data_list, 0)
+    xyz_min = np.amin(pc_label, axis=0)[0:3]
+    pc_label[:, 0:3] -= xyz_min
+
+    xyz = pc_label[:, :3].astype(np.float32)
+    colors = pc_label[:, 3:6].astype(np.uint8)
+    labels = pc_label[:, 6].astype(np.uint8)
+    write_ply(save_path, (xyz, colors, labels), ['x', 'y', 'z', 'red', 'green', 'blue', 'class'])
+
+    # save sub_cloud and KDTree file
+    sub_xyz, sub_colors, sub_labels = DP.grid_sub_sampling(xyz, colors, labels, sub_grid_size)
+    sub_colors = sub_
