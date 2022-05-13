3d23e7e9da304ff5088de8048a1821666016b98e
VibeVision <VibeVisio80@proton.me>
2022-05-13 16:36:30 +0000

    rm unused import

--- SSRD_AL_semantic3d/utils/data_prepare_semantickitti.py
+++ SSRD_AL_semantic3d/utils/data_prepare_semantickitti.py
@@ -39,4 +39,39 @@ for seq_id in seq_list:
         label_path_out = join(seq_path_out, 'labels')
         os.makedirs(label_path_out) if not exists(label_path_out) else None
         scan_list = np.sort(os.listdir(pc_path))
-        for scan_id in scan_lis
+        for scan_id in scan_list:
+            print(scan_id)
+            points = DP.load_pc_kitti(join(pc_path, scan_id))
+            labels = DP.load_label_kitti(join(label_path, str(scan_id[:-4]) + '.label'), remap_lut)
+            sub_points, sub_labels = DP.grid_sub_sampling(points, labels=labels, grid_size=grid_size)
+            search_tree = KDTree(sub_points)
+            KDTree_save = join(KDTree_path_out, str(scan_id[:-4]) + '.pkl')
+            np.save(join(pc_path_out, scan_id)[:-4], sub_points)
+            np.save(join(label_path_out, scan_id)[:-4], sub_labels)
+            with open(KDTree_save, 'wb') as f:
+                pickle.dump(search_tree, f)
+            if seq_id == '08':
+                proj_path = join(seq_path_out, 'proj')
+                os.makedirs(proj_path) if not exists(proj_path) else None
+                proj_inds = np.squeeze(search_tree.query(points, return_distance=False))
+                proj_inds = proj_inds.astype(np.int32)
+                proj_save = join(proj_path, str(scan_id[:-4]) + '_proj.pkl')
+                with open(proj_save, 'wb') as f:
+                    pickle.dump([proj_inds], f)
+    else:
+        proj_path = join(seq_path_out, 'proj')
+        os.makedirs(proj_path) if not exists(proj_path) else None
+        scan_list = np.sort(os.listdir(pc_path))
+        for scan_id in scan_list:
+            print(scan_id)
+            points = DP.load_pc_kitti(join(pc_path, scan_id))
+            sub_points = DP.grid_sub_sampling(points, grid_size=0.06)
+            search_tree = KDTree(sub_points)
+            proj_inds = np.squeeze(search_tree.query(points, return_distance=False))
+            proj_inds = proj_inds.astype(np.int32)
+            KDTree_save = join(KDTree_path_out, str(scan_id[:-4]) + '.pkl')
+            proj_save = join(proj_path, str(scan_id[:-4]) + '_proj.pkl')
+            np.save(join(pc_path_out, scan_id)[:-4], sub_points)
+            with open(KDTree_save, 'wb') as f:
+                pickle.dump(search_tree, f)
+            with open(proj_save, 'wb') as f:
